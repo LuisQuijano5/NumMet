73fcdf82163815af9f23a72683ca4a86249d0232
Luis Quijano <luis5.quijano5@gmail.com>
2024-03-07 11:15:27 -0600

    just skeleton

---
+++ Modules/main.py
@@ -0,0 +1,14 @@
+import sys
+from PySide6 import QtWidgets
+
+class MainWindow(QtWidgets.QMainWindow):
+    pass
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
